80fa0ded2a13af2e4a47eadbbc9054b0bab3f274
Devid Grinberg <dg.devid@gmail.com>
2023-06-06 14:54:23 +0200

    api post request removed, also tests written for testing API

--- yana/api/api.py
+++ yana/api/api.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 # Initialize the FastAPI app
 app = FastAPI()
-
-# Define a Pydantic model, which is used for request validation and serialization
-class Item(BaseModel):
-    text: str
 
 # Define a GET endpoint at the root ("/") of the API
 @app.get("/")
@@ -14,15 +9,6 @@ def read_root():
     # This function will be called when a GET request is made to "/"
     # Returns a simple JSON response
     return {"Welcome to": "YANA"}
-
-# Define a POST endpoint at "/text"
-@app.post("/text")
-def process_text(item: Item):
-    # This function will be called when a POST request is made to "/text"
-    # The 'item' parameter is automatically populated from the JSON body of the request
-    # For now, it simply returns the input as output
-    result = item.text
-    return {"result": result}
 
 # Define a GET endpoint at "/predict"
 @app.get("/predict")
