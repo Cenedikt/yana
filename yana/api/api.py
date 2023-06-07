62442967a26bae1072686b083632123579247593
Devid Grinberg <dg.devid@gmail.com>
2023-06-07 12:15:38 +0200

    API simplified, predict taken out, buut dockerized and running correctly via docker on localhost: next step - GCR ## files sorted and stuff removed (ipynb checkpoints und so :)

--- yana/api/api.py
+++ yana/api/api.py
@@ -11,13 +11,25 @@ def read_root():
     return {"Welcome to": "YANA"}
 
 # Define a GET endpoint at "/predict"
-@app.get("/predict")
-def predict(
-    query: str,
-    ):
+# @app.get("/predict")
+# def predict(
+#     query: str,
+#     ):
+#     # This function will be called when a GET request is made to "/predict"
+#     # The 'query' parameter is automatically populated from the query parameters of the request
+#     # For now, it simply returns the input as output
+#     return {
+#         "query": query,
+#     }
+
+
+# Define a GET endpoint at "/predict"
+@app.get("/docker_test")
+def docker_test():
+
     # This function will be called when a GET request is made to "/predict"
     # The 'query' parameter is automatically populated from the query parameters of the request
     # For now, it simply returns the input as output
     return {
-        "query": query,
-    }
+        "This is": "docker",
+        }
