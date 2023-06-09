b052d1580621f0f660b3355ac6e0e433da92a8a7
emanuel patachi <emanuelpatachi@gmail.com>
2023-06-09 17:35:53 +0200

    new Frontend with gcloud

--- yana/Frontend/app.py
+++ yana/Frontend/app.py
@@ -34,24 +34,19 @@ def set_background(png_file):
         font-size: 20px;
         font-weight: bold;
     }
-
     #MainMenu {visibility: hidden; }
     footer {visibility: hidden;}
-
     </style>
     ''' % bin_str
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
 img = Image.open('/home/emanuel/code/cenedikt/yana/yana/Frontend/Content/Yana_background_image.png')
-
 
 st.set_page_config(page_title='YANA', page_icon=img)
 
 def main():
-    set_background('/home/dizziebeatz/code/Cenedikt/yana/yana/Frontend/Content/Yana_background_image.png')
-
+    set_background('/home/emanuel/code/cenedikt/yana/yana/Frontend/Content/Yana_background_image.png')
 
     st.markdown('''
     <style>
@@ -75,6 +70,7 @@ def main():
         text-align: center;
     }
     .result-item {
+        margin-bottom: 10px;
         background-color: rgba(242, 242, 242, 0.3);
         padding: 20px;
         border-radius: 10px;
@@ -86,16 +82,13 @@ def main():
     st.markdown("<div class='title-box'><h1 style='text-align: center;'>YANA: You are not alone</h1></div>", unsafe_allow_html=True)
     st.markdown("<div class='description-box'><h3 style='text-align: center;'>Welcome to our Mental Health Platform powered by NLP</h3><p style='text-align: center;'>We use advanced technology to analyze data from popular mental health subreddits and provide valuable insights. Our platform connects individuals with similar needs, fostering a sense of community and support. We offer community-assessed solutions and a comprehensive overview of prevalent mental health struggles. Join us as we leverage technology and shared experiences to create a more empathetic and inclusive mental health landscape.</p></div>", unsafe_allow_html=True)
 
-
     mode = st.radio("", ["Query", "Fetch Similar Posts"])
 
     if mode == "Query":
         query = st.text_input("Enter your query:")
         if st.button("Submit"):
-
-            url = "http://127.0.0.1:8000/query/"
+            url = "https://yanaapii-pg2zxhxypa-ey.a.run.app/query/"
             headers = {'Content-Type': 'application/json'}
-
             data = {"text": query}
             json_data = json.dumps(data)
 
@@ -103,15 +96,16 @@ def main():
 
             if response.status_code == 200:
                 results = response.json()
-
                 st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 14px; }</style>", unsafe_allow_html=True)
                 st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 18px; }</style>", unsafe_allow_html=True)
                 st.markdown("<div class='results-box'><h3>According to our model, the following posts are similar to your query:</h3></div>", unsafe_allow_html=True)
+                results_html = ""
                 for result in results['text']:
-                    st.write(f"<div class='result-item'>• {result}</div>", unsafe_allow_html=True)
+                    results_html += "<div class='result-item'>• " + result + "</div>"
+
+                    st.markdown(results_html, unsafe_allow_html=True)
             else:
                 st.error("There was an error processing your query.")
-
 
     elif mode == "Fetch Similar Posts":
         similar_query = st.text_input("Enter your query:")
@@ -122,12 +116,10 @@ def main():
                 similar_posts = ["Example Post 1", "Example Post 2", "Example Post 3"]
                 st.spinner()
 
-
                 st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 14px; }</style>", unsafe_allow_html=True)
                 st.markdown("<div class='results-box'><h3>Similar Posts:</h3></div>", unsafe_allow_html=True)
-
-                for post in similar_posts:
-                    st.write(f"<div class='result-item'>{post}</div>", unsafe_allow_html=True)
+                posts_html = "<div class='result-item'>" + "<br>".join(similar_posts) + "</div>"
+                st.markdown(posts_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
