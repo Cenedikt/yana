bedad0dcde57d8f5e2a14fb06bdd59f4570590e7
Benedict Goldhorn <benedict.goldhorn@gmail.com>
2023-06-13 11:14:54 +0200

    added prep with converting auther into string

--- yana/data/preprocessor.py
+++ yana/data/preprocessor.py
@@ -6,9 +6,9 @@ from colorama import Fore, Style
 class Preprocessor:
 
     def __init__(self):
-        pass
+        self.hallo = 'hallo'
 
-    def preprocessor_post(data: pd.DataFrame) -> pd.DataFrame:
+    def preprocessor_post(self, data: pd.DataFrame) -> pd.DataFrame:
         '''
         removes all post where the lengt of th ebody is smaler than 72 chars and
         if the body is emty if the title is smaler then 72 chars
@@ -25,12 +25,13 @@ class Preprocessor:
                 if len(title) <= 72 :
                     df_ = data.drop(index)
             elif len(selftext) <= 72 :
-                df_ = data.drop(index)
+                data = data.drop(index)
 
         print(f"✅ Data has been cleaned")
-        return df_
+        data.author = data.author.map(lambda x : str(x))
+        return data
 
-    def preprocessor_post(data: pd.DataFrame) -> pd.DataFrame:
+    def preprocessor_comments(self, data: pd.DataFrame) -> pd.DataFrame:
         '''
         removes all comments where the lengt of the body is smaler than 72 chars and
         Params:
@@ -42,6 +43,16 @@ class Preprocessor:
             body = row['body']
             if len(body) <= 72 :
                 df_ = data.drop(index)
-
+        df_.author = df_.author.map(lambda x : str(x))
         print(f"✅ Data has been cleaned")
         return df_
+
+if __name__ == '__main__' :
+    import pandas
+    path='/home/cenedikt/code/Cenedikt/yana/yana/data/depression_dataset_reddit_cleaned.csv'
+
+    df = pandas.read_csv(path)
+    prep = Preprocessor()
+    print(df)
+    df = prep.preprocessor_post(df)
+    print(df)
