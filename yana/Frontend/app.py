8c7b56cd7a528565827a5dfd4d4aa8bdf7ddd417
emanuel patachi <emanuelpatachi@gmail.com>
2023-06-13 11:19:28 +0200

    adjusted output for both model 1 and model 2 plus font changes for title

--- yana/Frontend/app.py
+++ yana/Frontend/app.py
@@ -11,6 +11,7 @@ script_path = os.path.abspath(__file__)
 parent_dir = os.path.dirname(os.path.dirname(script_path))
 yana_logo_path = os.path.join(parent_dir, 'Frontend', 'Content', 'Yana_logo.png')
 yana_background_path = os.path.join(parent_dir, 'Frontend', 'Content', 'yana_background.jpeg')
+bukhari_script_path = os.path.join(parent_dir, 'Frontend', 'Content', 'Bukhari_Script.ttf')
 
 # Call st.set_page_config() as the first Streamlit command
 st.set_page_config(page_title='YANA', page_icon=yana_logo_path)
@@ -55,7 +56,7 @@ def set_background(png_file):
 
 
 # Load the Bukhari Script font file
-with open("BukhariScript-Regular.ttf", "rb") as font_file:
+with open(bukhari_script_path, "rb") as font_file:
     font_data = font_file.read()
 
 # Create a base64 encoded string for the font
@@ -67,10 +68,11 @@ font_style = f"""
     font-family: 'Bukhari Script';
     src: url(data:font/ttf;base64,{font_base64});
 }}
-"""
 
-# Apply the font style using st.markdown
-st.markdown(f"<style>{font_style}</style>", unsafe_allow_html=True)
+.title-box h1 {{
+    font-family: 'Bukhari Script', sans-serif;
+}}
+"""
 
 def main():
     set_background(yana_background_path)
@@ -107,8 +109,11 @@ def main():
     }
     </style>
     ''', unsafe_allow_html=True)
+    # Apply the font style using st.markdown
+    st.markdown(f"<style>{font_style}</style>", unsafe_allow_html=True)
 
-    st.markdown("<div class='title-box'><h1 style='text-align: center; font-family: Bukhari Script;'>YANA: You are not alone</h1></div>", unsafe_allow_html=True)
+    # Display the title with the Bukhari Script font
+    st.markdown("<div class='title-box'><h1 style='text-align: center;'>Yana you are not alone</h1></div>", unsafe_allow_html=True)
     st.markdown("<div class='description-box'><h3 style='text-align: center;'>Welcome to our Mental Health Platform powered by NLP</h3><p style='text-align: center;'>We use advanced technology to analyze data from popular mental health subreddits and provide valuable insights. Our platform connects individuals with similar needs, fostering a sense of community and support. We offer community-assessed solutions and a comprehensive overview of prevalent mental health struggles. Join us as we leverage technology and shared experiences to create a more empathetic and inclusive mental health landscape.</p></div>", unsafe_allow_html=True)
 
     mode = st.radio("Select an option:", ["Fetch Similar Reddit Posts", "Get Advice*"])
@@ -142,18 +147,66 @@ def main():
             else:
                 st.error("There was an error processing your query.")
 
-    elif mode == "Get Advice*":
-        similar_query = st.text_input("Enter your query:")
+            # if response.status_code == 200:
+            #     results = response.json()
+            #     st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 14px; }</style>", unsafe_allow_html=True)
+            #     st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 18px; }</style>", unsafe_allow_html=True)
+            #     st.markdown("<h3 style='text-align: center;'>According to our Model, the following Reddit posts are similar to your query:</h3>", unsafe_allow_html=True)
+            #     for result in results['text']:
+            #         author = result['Author']
+            #            title = result['Title']
+            #            body = result['Body']
+            #            subreddit = result['Subreddit']
+            #            upvotes = result['Upvotes']
+
+
+            #         st.markdown('''
+            #             <div class="result-card">
+            #                 <h4>👤Username: {author}</h4>
+            #                 <h4>📌Title: {title}</h4>
+            #                 <p style="font-size: 20px;"><strong>📄Post:</strong> <span style="font-size: 18px;">{body}</span></p>
+            #                 <h4>/🇷🇪 Subreddit: {subreddit}</h4>
+            #                 <h4>👍Upvotes: {upvotes}</h4>
+            #             </div>
+            #         '''.format(author=result['Author'], title=result['Title'], body=result['Body'], subreddit=result['Subreddit'], upvotes=result['Upvotes']), unsafe_allow_html=True)
+            # else:
+            #     st.error("There was an error processing your query.")
+
+    if mode == "Get Advice*":
+        query = st.text_input("Enter your query:")
         if st.button("Submit"):
-            with st.spinner("Fetching similar posts to your query..."):
-                time.sleep(3)
+            url = "https://yanaapii-pg2zxhxypa-ey.a.run.app/query2/"
+            headers = {'Content-Type': 'application/json'}
+            data = {"text": query}
+            json_data = json.dumps(data)
 
-                similar_posts = ["Example Post 1", "Example Post 2", "Example Post 3"]
-                st.spinner()
+            response = requests.post(url, headers=headers, data=json_data)
 
-                st.write("Similar Posts:")
-                for post in similar_posts:
-                    st.write(post)
+            if response.status_code == 200:
+                results = response.json()
+                st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 14px; }</style>", unsafe_allow_html=True)
+                st.write("<style>div[role='main'] div[data-testid='stDecoration'] { font-size: 18px; }</style>", unsafe_allow_html=True)
+                st.markdown("<h3 style='text-align: center;'>According to our Model, here is the advice:</h3>", unsafe_allow_html=True)
+                for result in results['text']:
+                    author = result['Author']
+                    title = result['Title']
+                    body = result['Body']
+                    subreddit = result['Subreddit']
+                    upvotes = result['Upvotes']
+
+                    st.markdown('''
+                        <div class="result-card">
+                            <h4>👤Username: {author}</h4>
+                            <h4>📌Title: {title}</h4>
+                            <p style="font-size: 20px;"><strong>📄Post:</strong> <span style="font-size: 18px;">{body}</span></p>
+                            <h4>/🇷🇪 Subreddit: {subreddit}</h4>
+                            <h4>👍Upvotes: {upvotes}</h4>
+                        </div>
+                    '''.format(author=result['Author'], title=result['Title'], body=result['Body'], subreddit=result['Subreddit'], upvotes=result['Upvotes']), unsafe_allow_html=True)
+
+            else:
+                st.error("There was an error processing your query.")
+
 
 if __name__ == "__main__":
     main()
