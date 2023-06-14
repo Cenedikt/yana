8c20d73193a80e94bed9cd96fadc14a839524028
emanuel patachi <emanuelpatachi@gmail.com>
2023-06-14 13:13:05 +0200

    added a autorefresh limi

--- yana/Frontend/app.py
+++ yana/Frontend/app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import os
 import requests
 import base64
+from streamlit_autorefresh import st_autorefresh
 
+st_autorefresh(5000)
 
 
 def http_encoder(sentence: str) -> str :
@@ -215,6 +217,8 @@ def main():
                     '''.format(result=result), unsafe_allow_html=True)
             else:
                 st.error("There was an error processing your query.")
+
+
 
 if __name__ == "__main__":
     main()
