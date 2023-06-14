c7f56f8842893523cf86bb7f4d7777edebc9ea31
Vlad Nicolescu <vlad.nicolescu96@gmail.com>
2023-06-14 12:57:38 +0200

    Debugging model 1.2

--- yana/api/api_v2.py
+++ yana/api/api_v2.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from yana.models import Model1_1
+from yana.models import Model1_1, Model1_2
 
 app = FastAPI()
 
 model1_1 = Model1_1()
+model1_2 = Model1_2()
+
+@app.get('/query_2')
+def predict(query):
+    prediction = model1_2.advice(query)
+    return {'text' : prediction}
+
 @app.get('/query_1')
 def predict(query):
     prediction = model1_1.search(query)
