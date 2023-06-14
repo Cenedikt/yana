464f844bcdf502e7cab54a1d1545db4a81fd1b36
Benedict Goldhorn <benedict.goldhorn@gmail.com>
2023-06-14 14:56:26 +0200

    fixed scrapper

--- yana/api/api_v2.py
+++ yana/api/api_v2.py
@@ -8,11 +8,13 @@ model1_2 = Model1_2()
 
 @app.get('/query_2')
 def predict(query):
+    print(query)
     prediction = model1_2.advice(query)
     return {'text' : prediction}
 
 @app.get('/query_1')
 def predict(query):
+    print(query)
     prediction = model1_1.search(query)
     return {'text' : prediction}
 
